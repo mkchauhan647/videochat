d7b307267f3d0c60cb4e9688f018a99ddacbe637
mkchauhan647 <mkchauhan647@gmail.com>
2024-07-05 22:05:12 +0545

    initial

--- src/app/room1/page.tsx
+++ src/app/room1/page.tsx
@@ -1,14 +1,12 @@
 'use client'
 import { useEffect, useMemo, useState } from "react";
-import { useSearchParams } from "next/navigation";
 const Room1 = () => {
     const initialState = useMemo(() => ({ name: 'room' }), []);
-    const router = useSearchParams();
 
     const [state, setState] = useState(initialState);
 
     useEffect(() => {
-        console.log(router.get('token'));;
+        // console.log(router.get('token'));;
         console.log('fetching data');
     }, [state]);
 
@@ -48,7 +46,7 @@ const Room1 = () => {
             <h1>Hello I am room One</h1>
             <h2>State: {state.name}</h2>
             <h2 onClick={addCity}>Add City</h2>
-            <h2 onClick={handleOpenWindow}>Open Window</h2>
+            {/* <h2 onClick={handleOpenWindow}>Open Window</h2> */}
         </>
     );
 };
